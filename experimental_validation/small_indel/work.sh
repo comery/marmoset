56516e21e3c4fc5450f385bae7684d1559c5c2a0
comery <comery@foxmail.com>
2021-01-19 12:31:19 +0800

    empty

--- experimental_validation/small_indel/work.sh
+++
@@ -1,4 +0,0 @@
-blastn -query all.primer.fa -out all.primer.blastout -db /hwfssz5/ST_DIVERSITY/P18Z10200N0160_Hyena/USER/yangchentao/marmoset/2.genetic_divergency/SNP_INDEL/experiments/blastdb/maternal -outfmt 6  -max_target_seqs 5 -num_threads 8 -task blastn
-
-
-awk '($6>100 && $6<200) || ($6>350 && $6<450)' all.primer.blastout.good.sort.primers
--- sequencing_and_polishing_error/1.get_compared.tab.sh
+++
@@ -1,1 +0,0 @@
-cat list |while read a;do cd $a;echo "perl ../../bin/compare.snp.diff.pl $a.diff.snp $a.snp.vcf.gz $a.sambcf.vcf.gz $a $a.pileup $a.diff.1coords" >$a.work.sh; sel-qsub evo 20g 1 $a.work.sh;cd ..;done
